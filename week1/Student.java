e6a211a19309a578d699d7df324bc2b6e1d6298b
utmp <2210206543@ogrenci.karabuk.edu.tr>
2025-10-24 11:50:48 +0300

    completed week1

---
+++ week1/Student.java
@@ -0,0 +1,9 @@
+class Student{
+    int number; // attribute
+    String name; 
+    
+    // behaviour
+    void printInfo(){ 
+        System.out.printf("Student name: %s, number: %d",name,number);
+    }
+}
---
+++ week1/index.java
@@ -0,0 +1,8 @@
+public class index{
+    public static void main(String[] args){
+        Student student1 = new Student();
+        student1.name = "John";
+        student1.number = 8923;
+        student1.printInfo();
+    }
+}
