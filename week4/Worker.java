576d6dccbc055d12af21e7cca6766f28ae997196
utmp <2210206543@ogrenci.karabuk.edu.tr>
2025-10-30 11:59:36 +0300

    completed week4

--- week4/Worker.java
+++ week4/Worker.java
@@ -2,11 +2,16 @@ class Worker{
     String name;
     int salary;
     public static int counter;
-
+    final static String companyName = "Amazon"; // constant value
     void setSalary(){
 
     }
     void setName(){
         
+    }
+    public Worker(String name, int salary, int id){
+        this.name = name;
+        this.salary = salary;
+        this.counter = id;
     }
 }
--- week4/index.java
+++ week4/index.java
@@ -1,4 +1,14 @@
 public class index{
     public static void main(String[] args) {
+        Worker worker1 = new Worker("Dave",344,3);
+        System.out.println(worker1.companyName);
+        System.out.println(worker1.counter);
+        try{
+            int[] nums = {1,2,5};
+            System.out.println(nums[10]);
+        }
+        catch(Exception e){
+            System.out.printf("Something went wrong: %s",e);
+        }
     }
 }
